6a8c23f60f977035b9165e99f58237b377b7d8e1
Antonio Orozco <antonio@e-dolph.com>
2023-06-20 15:18:07 -0600

    no debería subir

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,4 @@ export const DB_PORT = 3306
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'MariaDBpwd'
 export const DB_DATABASE = process.env.DB_DATABASE || 'jwa_alliance'
+// test
