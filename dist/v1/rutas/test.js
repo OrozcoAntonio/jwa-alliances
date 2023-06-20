e8c308ce11d699cfcd79ab987e1aad3a4fdbc889
OrozcoAntonio <antonio@e-dolph.com>
2023-06-20 15:21:01 -0600

    Delete dist directory

--- dist/v1/rutas/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ctrlTest = void 0;
-const ctrlTest = (req, res) => {
-    res.status(200);
-};
-exports.ctrlTest = ctrlTest;
-//# sourceMappingURL=test.js.map
